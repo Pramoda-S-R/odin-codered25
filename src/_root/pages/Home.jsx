9d1baad231326ed476beba56539b19c9ea0e3b4d
Pramoda-S-R <pramoda9.2.2004@gmail.com>
2025-01-10 00:12:25 +0530

    self harm incoming

--- src/_root/pages/Home.jsx
+++ src/_root/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import WebSocketConnection from "../../components/WebSocketConnection";
+import AudioVideoComponent from "../../components/AudioVideoComponent";
 
 const Home = () => {
   return (
     <div className="flex flex-1">
       Home
+      <WebSocketConnection url="ws://localhost:9082" />
     </div>
   );
 };
